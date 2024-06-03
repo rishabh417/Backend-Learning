10a252f04f06ab6a2328095299fac5b23e1fbcc9
rishabh417 <rishusingh417@gmail.com>
2024-06-03 13:05:18 +0530

    used cors and middlewares

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 // require('dotenv').config({path : './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import express from "express"
+
+const app = express()
 
 dotenv.config({
     path : './env'
 })
 connectDB()
+.then(() => {
+    app.on("error", (error)=>{
+        console.log("Error : ", error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("error in index.js in src"+error)
+    throw error
+})
