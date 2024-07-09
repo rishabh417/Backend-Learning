e0da3fe194176179ecc6aae96c5403bcae285797
rishabh417 <rishusingh417@gmail.com>
2024-07-09 13:48:44 +0530

    used bcrypt in user.model.js for hashing the password and used jwt token in usermodel .

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
     userName : {
@@ -45,5 +47,35 @@ const userSchema = new Schema({
     
 },{timestamps : true})
 
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password,10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign({
+        _id:this._id,
+        fullName : this.fullName,
+        email : this.email,
+        userName : this.userName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn : process.env.ACCESS_TOKEN_EXPIRY}
+)
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign({
+        _id:this._id,       
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {expiresIn : process.env.REFRESH_TOKEN_EXPIRY}
+)
+}
 
 export const User = mongoose.model("User",userSchema)
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
 
 const videoSchema = new Schema({
 
@@ -41,5 +43,7 @@ const videoSchema = new Schema({
 
         
 },{timestamps : true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model("Video",videoSchema)
