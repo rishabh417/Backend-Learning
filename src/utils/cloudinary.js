5f09100642b688f59f749f6a3ca2ea501ef4e420
rishabh417 <rishusingh417@gmail.com>
2024-07-10 00:54:54 +0530

    introduced cloudinary for keeping the uploaded  file and used multer  for file upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+cloudinary.config({ 
+    cloud_name:CLOUDINARY_CLOUD_NAME, 
+    api_key: CLOUDINARY_API_KEY, 
+    api_secret: CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localpath) => {
+    if(!localpath) return null
+    try {
+        const response = await cloudinary.uploader
+       .upload(
+           localpath, {
+               resource_type : "auto"
+           }
+       )
+    
+    console.log("file is uploaded on cloudinary " + response);
+    return response
+
+    } catch (error) {
+        fs.unlink(localpath) // removes the file from local server in case of failed attempt to upload it.
+        return null
+    }
+
+}
+
+
